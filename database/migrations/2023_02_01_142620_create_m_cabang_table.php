11d9000898045ca8f027f7dc75b88981c90457da
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-02-01 22:00:20 +0700

    pertemuan 7: migrasi database 6 table

---
+++ database/migrations/2023_02_01_142620_create_m_cabang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMCabangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_cabang', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_cabang', 3);
+            $table->string('nama_cabang');
+            $table->string('alamat');
+            $table->string('no_telp', 14);
+            $table->dateTime('created_date');
+            $table->dateTime('updated_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_cabang');
+    }
+}
