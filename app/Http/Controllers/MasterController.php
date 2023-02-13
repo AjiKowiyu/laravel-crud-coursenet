bc12525eea1dfe80239b1fadcb6df5b6ee34ed90
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-02-13 20:24:39 +0700

    pertemuan 10: simpan cabang

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class MasterController extends Controller
 {
@@ -28,6 +29,30 @@ class MasterController extends Controller
     {
         return view('master.cabang.tambah');
     }
+
+
+
+    public function cabang_simpan(Request $request)
+    {
+        try {
+            $insert = DB::table('m_cabang')->insert([
+                'kode_cabang'   => $request->input('form_kode_cabang'),
+                'nama_cabang'   => $request->input('form_nama_cabang'),
+                'no_telp'       => $request->input('form_notelp_cabang'),
+                'alamat'        => $request->input('form_alamat_cabang'),
+                'created_date'  => date('Y-m-d H:i:s'),
+            ]);
+            if ($insert) {
+                echo 'Berhasil tambah cabang restoran baru';
+            } else {
+                echo 'Gagal bos';
+            }
+        } catch (\Throwable $th) {
+            print_r($th->getMessage);
+        }
+        
+    }
+
 
 
     public function menuresto()
